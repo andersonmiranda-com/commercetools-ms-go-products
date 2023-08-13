786a3884b7ab75d96b619be55ed0a7ac9bcc58c5
Anderson Miranda <me@andersonmiranda.com>
2023-08-13 22:22:37 +0200

    Add Create/Update/Publish/Unpublish (#6)
    Co-authored-by: Anderson Miranda <amiranda@dminc.com>

--- controller/controller.go
+++ controller/controller.go
@@ -3,10 +3,12 @@ package controller
 import (
 	"commercetools-ms-product/service"
 	"commercetools-ms-product/utils"
+	"encoding/json"
 	"net/http"
 	"strconv"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/labd/commercetools-go-sdk/platform"
 )
 
 func Get(c *fiber.Ctx) error {
@@ -68,6 +70,139 @@ func Find(c *fiber.Ctx) error {
 
 	if withTotal, err := strconv.ParseBool(withTotalStr); err == nil {
 		productsGetter = productsGetter.WithTotal(withTotal)
+	}
+
+	productResults, err := productsGetter.Execute(ctx)
+	return utils.Response(productResults, http.StatusOK, err, c)
+}
+
+func Create(c *fiber.Ctx) error {
+
+	expand := c.Query("expand")
+
+	productDraft := platform.ProductDraft{}
+
+	if err := c.BodyParser(&productDraft); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Cannot parse ProductDraft",
+		})
+	}
+
+	projectClient, ctx := service.Connector()
+	productsGetter := projectClient.Products().Post(productDraft)
+
+	if expand != "" {
+		if expandSlice, err := utils.ConvertToSlice(expand); err == nil {
+			productsGetter = productsGetter.Expand(expandSlice)
+		}
+	}
+
+	productResults, err := productsGetter.Execute(ctx)
+	return utils.Response(productResults, http.StatusOK, err, c)
+}
+
+func Update(c *fiber.Ctx) error {
+
+	id := c.Params("id")
+	expand := c.Query("expand")
+
+	productUpdate := platform.ProductUpdate{}
+
+	if err := c.BodyParser(&productUpdate); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Cannot parse ProductUpdate",
+		})
+	}
+
+	projectClient, ctx := service.Connector()
+	productsGetter := projectClient.Products().WithId(id).Post(productUpdate)
+
+	if expand != "" {
+		if expandSlice, err := utils.ConvertToSlice(expand); err == nil {
+			productsGetter = productsGetter.Expand(expandSlice)
+		}
+	}
+
+	productResults, err := productsGetter.Execute(ctx)
+	return utils.Response(productResults, http.StatusOK, err, c)
+}
+
+func Publish(c *fiber.Ctx) error {
+
+	id := c.Params("id")
+	expand := c.Query("expand")
+
+	// Get oldProduct version
+	projectClient, ctx := service.Connector()
+	oldProduct, err := projectClient.Products().WithId(id).Get().Execute(ctx)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Cannot parse ProductUpdate",
+		})
+	}
+
+	actionPayload := `{
+		"version" : ` + strconv.Itoa(oldProduct.Version) + `,
+		"actions" : [ {
+		  "action" : "publish"
+		} ]
+	  }`
+
+	productUpdate := platform.ProductUpdate{}
+
+	if err := json.Unmarshal([]byte(actionPayload), &productUpdate); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Cannot parse actionPayload",
+		})
+	}
+
+	productsGetter := projectClient.Products().WithId(id).Post(productUpdate)
+
+	if expand != "" {
+		if expandSlice, err := utils.ConvertToSlice(expand); err == nil {
+			productsGetter = productsGetter.Expand(expandSlice)
+		}
+	}
+
+	productResults, err := productsGetter.Execute(ctx)
+	return utils.Response(productResults, http.StatusOK, err, c)
+}
+
+func Unpublish(c *fiber.Ctx) error {
+
+	id := c.Params("id")
+	expand := c.Query("expand")
+
+	// Get oldProduct version
+	projectClient, ctx := service.Connector()
+	oldProduct, err := projectClient.Products().WithId(id).Get().Execute(ctx)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Cannot parse ProductUpdate",
+		})
+	}
+
+	actionPayload := `{
+		"version" : ` + strconv.Itoa(oldProduct.Version) + `,
+		"actions" : [ {
+		  "action" : "unpublish"
+		} ]
+	  }`
+
+	productUpdate := platform.ProductUpdate{}
+
+	if err := json.Unmarshal([]byte(actionPayload), &productUpdate); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Cannot parse actionPayload",
+		})
+	}
+
+	productsGetter := projectClient.Products().WithId(id).Post(productUpdate)
+
+	if expand != "" {
+		if expandSlice, err := utils.ConvertToSlice(expand); err == nil {
+			productsGetter = productsGetter.Expand(expandSlice)
+		}
 	}
 
 	productResults, err := productsGetter.Execute(ctx)
