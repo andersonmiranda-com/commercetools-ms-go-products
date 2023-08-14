2a75e2f3c98d17fced64ccbaa7f399c4ebe36bf2
Anderson Miranda <amiranda@dminc.com>
2023-08-14 13:27:02 +0200

    Some cleanup and unit tet

--- utils/utils.go
+++ utils/utils.go
@@ -2,82 +2,10 @@ package utils
 
 import (
 	"net/http"
-	"strings"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/labd/commercetools-go-sdk/platform"
 )
-
-func GetWhereClause(c *fiber.Ctx) []string {
-
-	name := c.Query("name")
-	slug := c.Query("slug")
-	key := c.Query("key")
-	locale := c.Query("locale")
-	if locale == "" {
-		locale = "en"
-	}
-
-	where := generateWhereClause(name, slug, key, locale)
-
-	// Collecting other query parameters as strings
-	whereClause := []string{}
-	excludedKeys := map[string]bool{
-		"name":      true,
-		"slug":      true,
-		"key":       true,
-		"locale":    true,
-		"expand":    true,
-		"sort":      true,
-		"limit":     true,
-		"offset":    true,
-		"withTotal": true,
-	}
-
-	c.Request().URI().QueryArgs().VisitAll(func(k, v []byte) {
-		key := string(k)
-		if !excludedKeys[key] {
-			whereClause = append(whereClause, key+"="+string(v))
-		}
-	})
-
-	if len(where) > 0 {
-		whereClause = append(whereClause, where...)
-	}
-
-	return whereClause
-}
-
-func generateWhereClause(name, slug, key, locale string) []string {
-	var where []string
-
-	if name != "" {
-		where = append(where, "masterData(current(name("+locale+"=\""+name+"\")))")
-	}
-
-	if key != "" {
-		where = append(where, "key=\""+key+"\"")
-	}
-
-	if slug != "" {
-		where = append(where, "masterData(current(slug("+locale+"=\""+slug+"\")))")
-	}
-
-	return where
-}
-
-func ConvertToSlice(input string) ([]string, error) {
-	if strings.HasPrefix(input, "[") && strings.HasSuffix(input, "]") {
-		trimmed := strings.Trim(input, "[]")
-		resultSlice := strings.Split(trimmed, ",")
-		for i, val := range resultSlice {
-			resultSlice[i] = strings.TrimSpace(val) // remove potential whitespace
-		}
-		return resultSlice, nil
-	}
-
-	return []string{input}, nil
-}
 
 func Response(data interface{}, httpStatus int, err error, c *fiber.Ctx) error {
 
